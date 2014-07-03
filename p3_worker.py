0a56cfcdaa41deb49fad1b25c389249220113c3e
winit <winittheanvanichpant@winits-MacBook-Pro.local>
2014-07-03 23:37:04 +0700

    can receive work from server, no ping yet

--- p3_worker.py
+++ p3_worker.py
@@ -7,17 +7,24 @@ import socket as s
 import time 
 
 class Worker(threading.Thread):
-	def __init__(self,start,end,hash):
+	def __init__(self, startRange, endRange, hashValue):
+		print "~~~ worker instance instanciate ~~~"
 		threading.Thread.__init__(self)
-		self.start = start
-		self.end = end
-		self.hash = hash
+		self.startRange = int(startRange)
+		self.endRange = int(endRange)
+		self.hashValue = hashValue
+		print self.startRange
+		print self.endRange
+		print self.hashValue
 
 	def run(self):
-		answer = crack(self.start, self.end, self.hash)
-		return answer
+		print "~~~ into cracking password ~~~"
+		answer = self.crack(self.startRange, self.endRange, self.hashValue)
+		print "~~~ get the answer ~~~"
+		print answer
+		return 
 
-	def convert(no, al=0):
+	def convert(self, no, al=0):
 		alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
 		if(no < 62):
 			return alphabet[no]
@@ -25,17 +32,15 @@ class Worker(threading.Thread):
 			al = no % 62
 			no = no // 62
 			if (no == 0):
-				no = 0
-			else:
 				no = no - 1
-			return  convert(no,al)+ alphabet[al]
+			return  self.convert(no,al)+ alphabet[al]
 
-	def crack(start,end,hash):
-		status = "ND"
-		for i in range(start,end+1):
-			if(crypt(convert(i), "ic") == hash):
-				return "DF:" + convert(i)
-		status = "NF:" + hash
+	def crack(self, startRange, endRange, hashValue):
+		status = "nd"
+		for i in range(startRange,endRange+1):
+			if(crypt(self.convert(i), "ic") == hashValue):
+				return "df:" + self.convert(i)
+		status = "nf:" + hashValue
 		return status
 
 
@@ -47,32 +52,44 @@ class WorkerClient(threading.Thread):
 		self.port = port
 
 	def run(self):
-		self.connSocket.sendto("rw", (host, port))
+		self.connSocket.sendto("rw", (self.host, self.port))
 		status = ""
 		while True:
 			buf, address = self.connSocket.recvfrom(1024)
+			print "~~~ message from server ~~~"
+			print buf
 			if buf == "rs":
 				print "have connection with server"
 				status = ""
 			elif buf[:2] == "as":
 				print "server assign task to worker"
-				self.connSocket.sendto("wa",(host, port))
+				self.connSocket.sendto("wa",(self.host, self.port))
 				l = buf.split(":")
 				s = l[1]
 				e = l[2]
 				h = l[3]
 				w = Worker(s,e,h)
-				status = w.start()
+				status = "nd"
+				print "~~~ before start crack pass ~~~"	
+				w.start()
+				print "~~~ after start crack pass ~~~"	
 			elif buf[:2] == "ps":
 				print "server ask worker for work progress"
-				self.connSocket.sendto(status,(host, port))
+				self.connSocket.sendto(status,(self.host, self.port))
 			elif buf[:2] == "kp":
 				print "kill worker process"
 				w.exit()
-				self.connSocket.sendto("kp",(host, port))
+				self.connSocket.sendto("kp",(self.host, self.port))
+			else:
+				print "~~~ something crap! ~~~"
+
 if __name__ == '__main__':
 	serverPort = 3333
 	hostIP = "169.254.182.174"
 	clientSocket = s.socket(s.AF_INET, s.SOCK_DGRAM)
-	thread = WorkerClient(clientSocket,hostIP,serverPort)
-	thread.start()	
+	clientSocket("rw", (hostIP, serverPort))
+	while True:
+		buf, address = clientSocket.recvfrom(1024)
+	process = WorkerClient(clientSocket,hostIP,serverPort)
+	process.listenFromServer()
+	# 169.254.223.238
