b86d37a0720e90f1c53df2ce32053be28661c5cd
Fuoon <ou_satinat@hotmail.com>
2014-07-03 18:59:27 +0700

    Sending data

--- p3.py
+++ p3.py
@@ -4,9 +4,14 @@ import threading
 import time
 from collections import deque
 
+total_com = 57731386986
+start = 0
+end = 10000
+
 class Singleton(object):
 	_instance = None
 	cli_queue = []
+	workers = []
 
 	def __new__(cls, *args, **kwargs):
 		if not cls._instance:
@@ -16,72 +21,42 @@ class Singleton(object):
 	def getCliQueue():
 		return self.cli_queue
 
-class Bundle():
-	def __init__(self):
-		self.cliAddr = ''
-		self.hashValue = ''
-		self.Worker = None
-		self.Workers = []
+	def getWorkers():
+		return self.workers
 
-	def deleteWorker(self, workerAddr):
-		self.Workers.remove(workerAddr)
+class Client():
+	def __init__(self, addr, hashValue):
+		self.addr = addr
+		self.hashValue = hashValue
 
-	def addWorker(self, worker):
-		self.Workers.append(self.Worker)
-
-	def createWorker(self, workerAddr):
-		self.Worker = Worker(workerAddr)
-
-	def getWorkerList(self):
-		return self.Workers
-
-	def getCliAddr(self):
-		return self.cliAddr
-
-	def getHashValue(self):
-		return self.hashValue
-
-	def getWorkerAddr(self):
-		return self.Worker.getAddr()
-
-	def getStart(self):
-		return self.Worker.getStart()
-
-	def getEnd(self):
-		return self.Worker.getEnd()
-
-	def setCliAddr(self, cliAddr):
-		self.cliAddr = cliAddr
+	def setAddr(self, addr):
+		self.addr = addr 
 
 	def setHashValue(self, hashValue):
 		self.hashValue = hashValue
 
-	def setWorkerAddr(self, workerAddr):
-		self.Worker.setAddr(workerAddr)
-
-	def setStart(self, start):
-		self.Worker.setStart(start)
-
-	def setEnd(self, end):
-		self.Worker.setEnd(end)
-
-class Worker():
-	def __init__(self, addr):
-		self.addr = addr
-		self.start = None
-		self.end = None
-
 	def getAddr(self):
 		return self.addr 
 
-	def getStart(self):
-		return self.start
+	def gethashValue(self):
+		return self.hashValue
 
-	def getEnd(self):
-		return self.end
+class Worker():
+	def __init__(self, addr, status):
+		self.addr = addr
+		self.status = status
+		self.hashValue = hashValue
+		self.start = 0  
+		self.end = 0
 
 	def setAddr(self, addr):
 		self.addr = addr
+
+	def setStatus(self, status):
+		self.status = status
+
+	def setHashValue(self, hashValue):
+		self.hashValue = hashValue
 
 	def setStart(self, start):
 		self.start = start
@@ -89,6 +64,50 @@ class Worker():
 	def setEnd(self, end):
 		self.end = end
 
+	def getAddr(self):
+		return self.getAddr
+
+	def getStatus(self):
+		return self.status
+
+	def getHashValue(self):
+		return self.hashValue
+
+	def getStart(self):
+		return self.start 
+
+	def getEnd(self):
+		return self.end
+
+class TransferHandler():
+	def __init__(self, data):
+		self.data = data
+
+	def run(self):
+		global start
+		global end
+		st = Singleton()
+		workers = st.getWorkers()
+		for i in workers:
+			addr = i.getAddr()
+			data = "as:" + self.start + ":" + self.end + ":" + self.data
+			sock = s.socket(s.AF_INET, s.SOCK_DGRAM)
+			sock.sendto(data, (addr))
+			i.setStatus("busy")
+			start += 10000
+			end += 10000
+			i.setStart(start)
+			i.setEnd(end)
+			i.setHashValue(self.data)
+
+class Manager(threading.Thread):
+	def __init(self):
+		threading.Thread.__init__(self)
+
+	def range_manager(self):
+		global total_com
+		chunk = int(total_com/8)
+		
 class HandleClientConnection(threading.Thread):
 	def __init__(self, data, addr, serverSocket):
 		threading.Thread.__init__(self)
@@ -98,7 +117,16 @@ class HandleClientConnection(threading.Thread):
 
 	def run(self):
 		if self.data:
-			self.serverSocket.sendto("Welcome, please wait while we are trying to crack the password!!!", self.addr)
+			st = Singleton()
+			client = Client(self.addr, self.data.split(":")[1])
+			clients = st.getCliQueue()
+			clients.append(client)
+			workers = st.getWorkers()
+			if workers:
+				TransferHandler(self.data.split(":")[1])
+			else:
+				self.serverSocket.sendto("Currently the system is not avaliable, please try again later")
+				return
 		else:
 			print "Good bye", self.addr
 			self.serverSocket.close()
@@ -113,11 +141,30 @@ class HandleWorkerConnection(threading.Thread):
 	def run(self):
 		if self.data:
 			self.serverSocket.sendto("rs", self.addr)
+			st = Singleton()
+			workers = st.getWorkers()
+			worker = Worker(self.addr, "free")
+			workers.append(worker)
+			return
 		else:
 			print "Good bye", self.addr
 			self.serverSocket.close()
 
-class HandleWorkerNewJob(threading.Thread):
+class HandleWorkerDoneNotFound(threading.Thread):
+	def __init__(self, data, addr, serverSocket):
+		threading.Thread.__init__(self)
+		self.data = data
+		self.addr = addr
+		self.serverSocket = serverSocket
+		self.hash = data.split(":")[1]
+
+	def run(self):
+		global start
+		global end 
+		data = "as:" + start + ":" + end ":" + self.hash 
+		self.serverSocket.sendto(data, (self.addr))
+
+class HandleWorkerDoneFound(threading.Thread):
 	def __init__(self, data, addr, serverSocket):
 		threading.Thread.__init__(self)
 
@@ -130,9 +177,12 @@ if __name__ == '__main__':
 		if data[:2] == "rw":
 			thread = HandleWorkerConnection(data, addr, serverSocket)
 			thread.start()
-		elif data[:2] == "cr":
+		elif data[:2] == "cp":
 			thread = HandleClientConnection(data, addr, serverSocket)
 			thread.start()
 		elif data[:2] == "nf":
-
+			thread = HandleWorkerDoneNotFound(data, addr, serverSocket)
+			thread.start()
 		elif data[:2] == "df":
+			thread = HandleWorkerDoneFound(data, addr, serverSocket)
+			thread.start()
