37932f0fad23ccbd4461655e716e99aa876082c1
winit <winittheanvanichpant@winits-MacBook-Pro.local>
2014-07-03 15:24:06 +0700

    change conversion method

--- p3_worker.py
+++ p3_worker.py
@@ -7,32 +7,48 @@ import socket as s
 import time 
 
 class Worker(threading.Thread):
-	def intToStr(n):
+	def __init__(self,start,end,hash):
+		threading.Thread.__init__(self)
+		self.start = start
+		self.end = end
+		self.hash = hash
+
+	def run(self):
+		answer = crack(self.start, self.end, self.hash)
+		return answer
+
+	def convert62(n):
+		def combination(i):
+			if i >= 1:
+				return (62 ** i) + combination(i-1)
+			else:
+				return (62 ** i)
+
+		def setString(no,iter,a):
+			if(iter == 1):
+				return str(a[no % 62])
+			elif(iter == 2):
+				return str(a[((no - 0) / 62) % 62 - 1])
+			else:
+				return str(a[((no - 62 ** (iter - 2)) / 62 ** (iter - 1)) % 62 - 1])
+
 		alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789'
+		s = ""
 		iterate = 1
-		str = ""
-		if(n >= 62):
-			temp = n % 62
-			str = str + alphabet[temp]
-			iterate += 1 
-			while True:
-				co = n - (n % 62 ** (iterate -1))
-				temp = (co / 62 ** (iterate - 1)) - 1
-				if (temp <= 61): 
-					str += alphabet[temp]
-					return str[::-1]
-				else:
-					str += alphabet[temp % 62]
-					iterate += 1
-		else:
-			return alphabet[n]
+		while True:
+			if(n < combination(iterate)-1):
+				s +=  setString(n,iterate,alphabet)
+				return s[::-1] 
+			else:
+				s += setString(n,iterate,alphabet)
+				iterate = iterate + 1
 
 	def crack(start,end,hash):
 		status = 0
 		for i in range(start,end+1):
-			if(crypt(intToStr(i), "ic") == hash):
-				return intToStr(i)
-		status = -1
+			if(crypt(convert62(i), "ic") == hash):
+				return convert62(i)
+		status = ""
 		return status
 
 
@@ -45,16 +61,26 @@ class WorkerClient(threading.Thread):
 
 	def run(self):
 		self.connSocket.sendto("rw", (host, port))
+		status = ""
 		while True:
 			buf, address = self.connSocket.recvfrom(1024)
 			if buf == "rs":
 				print "have connection with server"
+				status = ""
 			elif buf[:2] == "as":
 				print "server assign task to worker"
+				l = buf.split(":")
+				s = l[1]
+				e = l[2]
+				h = l[3]
+				w = Worker(s,e,h)
+				status = w.start()
 			elif buf[:2] == "ps":
-				print ""
+				print "server ask worker for work progress"
+				self.connSocket.sendto(status,(host, port))
 			elif buf[:2] == "kp":
-
+				print "kill worker process"
+				w.exit()
 
 if __name__ == '__main__':
 	serverPort = 3333
